a55e17904ebf3883b95708fff19f6b6605a16216
type0 <ghyghygg30@gmail.com>
2015-04-09 21:30:27 +0800

    40kHz and 80%,50%, 20% duty cycle done

--- lab2/jjv/jjv.ino
+++ lab2/jjv/jjv.ino
@@ -1,18 +1,23 @@
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
 
-int pin = 6;
+int pin = 6; // led pin for debug
 
-// the setup routine runs once when you press reset:
+// 16000 / 8 / 2 / 25 = 40kHz
 void setup() {                
-  pinMode(pin,OUTPUT);
+  pinMode(6,OUTPUT);        
+  TCCR3A = _BV(COM3B0);
+  TCCR3B = _BV(WGM32) | _BV(CS31);
+  OCR3A = 25;
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  send_bit1();
-  send_bit0();
-  send_marker_bit();
+//  analogWrite(pin,255);
+//  delay(800);
+//  analogWrite(pin,0);
+//  delay(200);
+//  send_bit1();
+//  send_bit0();
+//  send_marker_bit();
 }
 
 void send_bit0(){
@@ -20,7 +25,6 @@ void send_bit0(){
   delay(800);
   analogWrite(pin,LOW);
   delay(200);
-  
 }
 
 void send_bit1(){
