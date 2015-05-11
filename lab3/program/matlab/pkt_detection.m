34e74ada7e997ac164925e41a4f4f5f7bcbd32a5
type0 <ghyghygg30@gmail.com>
2015-05-11 16:41:08 +0800

    packet detection done

--- lab3/program/matlab/pkt_detection.m
+++ lab3/program/matlab/pkt_detection.m
@@ -7,7 +7,8 @@ lts_t = ifft(lts_f, 64);
 
 % Hint 1:
 % Complex cross correlation of Rx waveform with time-domain LTS 
-lts_corr = abs(conv(conj(fliplr(lts_t)), sign(raw_rx_dec)));
+lts_corr = abs(conv(conj(fliplr(lts_t)), sign(rx_ant)));
+% lts_corr = abs(xcorr(rx_ant,lts_t));
 
 % Skip early and late samples
 lts_corr = lts_corr(32:end-32);
@@ -15,3 +16,9 @@ lts_corr = lts_corr(32:end-32);
 % Hint 2:
 % Get the lts_ind and payload_ind
 % Don't forget that "You have skip early and late samples"
+peak = find(lts_corr>THRESH_LTS_CORR*max(lts_corr));
+[LTS1, LTS2] = meshgrid(peak,peak);
+[second_peak_index, y] = find(LTS2-LTS1 == length(lts_t));
+
+payload_ind = peak(max(second_peak_index))+64;
+lts_ind = payload_ind - LTS_LEN;
