15ba4a4e9cebda2257135ec7357d5559e7389e1a
type0 <ghyghygg30@gmail.com>
2015-05-13 15:37:26 +0800

    pkt detection

--- lab3/program/matlab/pkt_detection.m
+++ lab3/program/matlab/pkt_detection.m
@@ -1,4 +1,5 @@
 function [lts_ind payload_ind] = pkt_detection(rx_ant, THRESH_LTS_CORR)
+pkg load signal;
 LTS_LEN = 160;
 
 % Long preamble (LTS) for CFO and channel estimation
@@ -7,18 +8,30 @@ lts_t = ifft(lts_f, 64);
 
 % Hint 1:
 % Complex cross correlation of Rx waveform with time-domain LTS 
-lts_corr = abs(conv(conj(fliplr(lts_t)), sign(rx_ant)));
+% rx_ant = real(rx_ant).^2;
+% lts_corr = abs(conv(conj(fliplr(lts_t)), sign(rx_ant)));
+[lts_corr lag] = xcorr(rx_ant,lts_t);
 % lts_corr = abs(xcorr(rx_ant,lts_t));
 
 % Skip early and late samples
-lts_corr = lts_corr(32:end-32);
+% lts_corr = lts_corr(32:end-32);
+plot(abs(lts_corr));
 
 % Hint 2:
 % Get the lts_ind and payload_ind
 % Don't forget that "You have skip early and late samples"
 peak = find(lts_corr>THRESH_LTS_CORR*max(lts_corr));
-[LTS1, LTS2] = meshgrid(peak,peak);
-[second_peak_index, y] = find(LTS2-LTS1 == length(lts_t));
-
-payload_ind = peak(max(second_peak_index))+64;
-lts_ind = payload_ind - LTS_LEN;
+lag(peak(1))
+% [LTS1, LTS2] = meshgrid(peak,peak);
+% [second_peak_index, y] = find(LTS2-LTS1 == length(lts_t));
+% pick_peak = second_peak_index(1);
+% for i = 2:size(second_peak_index)
+% 	if second_peak_index(i) - pick_peak > 1
+% 		break;
+% 	end
+% 	pick_peak = second_peak_index(i);
+% end
+lts_ind = lag(peak(1)) - 32 + 1;
+payload_ind = lts_ind + 320;
+% payload_ind = peak(pick_peak)+32;
+% lts_ind = payload_ind - LTS_LEN;
