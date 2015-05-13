5f1a9e9eb687690eb86ca7ab6c85b29a7fae4741
type0 <ghyghygg30@gmail.com>
2015-05-13 17:43:34 +0800

    phase tracking (~w~)

--- lab3/program/matlab/phaseTrack.m
+++ lab3/program/matlab/phaseTrack.m
@@ -1,4 +1,5 @@
 function rx_out = phaseTrack(rx_in, tx_in, cf)
+	pkg load statistics;
 	% figure(cf) can use to plot process of phaseTrack
 	% TODO
 	
@@ -7,14 +8,23 @@ function rx_out = phaseTrack(rx_in, tx_in, cf)
     
 	% hint 1: find your pilot index according to signal_generator.m
     % >> pilot_idx = ___;
-    
-    phase_shift = phase( rx_buf(pilot_idx) ./ tx_buf(pilot_idx) );  %rx : ¤£°®²bªº(¯u¥¿¶Çªº¸ê®Æ)   tx : °®²bªº  %radian
-	
+    pilot_idx = [8 22 44 58];
+    X = [ones(size(pilot_idx)); pilot_idx];
+    phase_shift = angle( rx_buf(pilot_idx) ./ tx_buf(pilot_idx) );  %rx : Â¤Â£Â°Â®Â²bÂªÂº(Â¯uÂ¥Â¿Â¶Ã‡ÂªÂºÂ¸ÃªÂ®Ã†)   tx : Â°Â®Â²bÂªÂº  %radian
+
 	% hint 2: the phase shift is linear!
 	%         there is a matlab function called "regress"
     % phase_shift is in unit [radian] 
     % phase_shift_regressed = ___ ;  % should be complex values!!
 	
+    [B, bint, r, rint, stats]=regress(phase_shift,X');
+    % size(B)
+
+    phase_shift_regressed=B'*[1:64; rx_buf'];
+    phase_shift_regressed=abs(phase_shift_regressed).*exp(i*angle(phase_shift_regressed));
+
+
+
 	% hint 3: Use phase_shift_regressed to remove SFO
-	% rx_out = rx_in ./ ...
+		rx_out = rx_in ./ phase_shift_regressed'
 end
