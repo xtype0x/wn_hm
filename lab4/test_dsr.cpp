1247117bca4ac709c3a414e3236b07207a9c7c7d
type0 <ghyghygg30@gmail.com>
2015-05-28 00:57:58 +0800

    test file done

--- lab4/test_dsr.cpp
+++ lab4/test_dsr.cpp
@@ -7,6 +7,7 @@ int main(int argc, char const *argv[]){
 	DSR nodeS(1), nodeA(2), nodeB(3), nodeC(4), nodeD(5);
 	DsrPacket pkt;
 	int status;
+	cout<<sizeof(DsrPacket)<<endl;
 
 	pkt = nodeS.source(5);
 	pkt = nodeA.get_packet(&pkt);
@@ -17,11 +18,6 @@ int main(int argc, char const *argv[]){
 	pkt = nodeA.get_packet(&pkt);
 	pkt = nodeS.get_packet(&pkt);
 
-	cout<<"type:"<<pkt.type<<endl;
-	cout<<"length: "<<pkt.length<<endl;
-	cout<<"route:"<<endl;
-	for(int i=0;i<10;i++)cout<<pkt.route[i]<<" ";
-	cout<<endl;
 
 	return 0;
 }
